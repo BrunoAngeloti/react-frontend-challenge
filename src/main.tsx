ab5f3355208d6ea666d93f36ddd49eb89ef47871
brunoangeloti <brunoangelotip@gmail.com>
2026-04-15 16:50:11 -0300

    chore: bootstrap cinedash with vite, tailwind and base architecture

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+import "@/app/styles/globals.css";
+import { App } from "@/app/app";
+import { AppProvider } from "@/app/providers/app-provider";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </StrictMode>,
-)
+);
